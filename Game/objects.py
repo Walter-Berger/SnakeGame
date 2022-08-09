824581f5c9e3e83c08bcdff0282e1d481aaa8054
Walter Berger <W.berger89@gmx.at>
2022-08-09 15:19:56 +0200

    creating a blank screen

---
+++ Game/objects.py
@@ -0,0 +1,14 @@
+from mimetypes import init
+
+
+class Snake:
+    def __init__(self):
+        pass
+
+
+
+
+
+class Fruit:
+    def __init__(self):
+        pass
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from game import *
+from objects import *
 
 game = Game()
 
