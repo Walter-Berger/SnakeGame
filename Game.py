d0497bd2c4b58928c4ecf439b6d70553615bebf9
Walter Berger <W.berger89@gmx.at>
2022-08-09 15:09:49 +0200

    first steps

--- Game.py
+++ Game.py
@@ -1,5 +1,14 @@
-
+import pygame, sys
+pygame.init()
 
 class Game:
     def __init__(self):
-        pass
+        self.screen = pygame.display.set_mode((800, 800))
+        self.clock = pygame.time.Clock()
+
+    def update():
+        pygame.display.flip()
+
+    def quit():
+        pygame.quit()
+        sys.exit()
--- main.py
+++ main.py
@@ -1,19 +1,13 @@
-import pygame, sys
-
-pygame.init()
-
-screen = pygame.display.set_mode((800, 800))
-clock = pygame.time.Clock()
+from Game import *
 
 run = True
+game = Game()
 
 while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
     
-    pygame.display.flip()
-    clock.tick(60)
+    game.clock.tick(60)
 
-pygame.quit()
-sys.exit()
+game.quit()
